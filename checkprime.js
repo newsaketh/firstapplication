948b40e88f05fd32706c4435ca1a1ebc88c78494
newsaketh <30759939+newsaketh@users.noreply.github.com>
2017-09-14 21:14:43 -0400

    Create checkprime.js
    Checks whether a given number is prime or not

---
+++ checkprime.js
@@ -0,0 +1,25 @@
+function checkprime(value){
+   var flag=0;
+   if(value==1){
+   	console.log("one is neither prime nor composite");
+   	return;
+   }
+   for(var i=1;i<=value;i++){
+   	if(value%i==0){
+   		
+   	}
+   }
+   if(flag>2)
+   {
+   	console.log("Given number is not prime");
+   	console.log(flag);
+   }
+   else
+   {
+   	   	console.log("Given number is prime");
+
+   }
+
+
+
+}
